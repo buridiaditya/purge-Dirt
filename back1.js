5c6862f9793ac961f9a441aeea184bbc45eb21a0
Buridi Aditya <buridiaditya@gmail.com>
2017-01-14 16:31:24 +0530

    Functions of Cleaner and User added

--- back1.js
+++ back1.js
@@ -2,44 +2,52 @@ var x = document.getElementById("demo");
 
 	function getLocation() {
 	    if (navigator.geolocation) {
-	        navigator.geolocation.getCurrentPosition(initMap);
+	        navigator.geolocation.getCurrentPosition(saveCookieAndSave);
 	    } else { 
 	        x.innerHTML = "Geolocation is not supported by this browser.";
 	    }
 	}
-
-
-// function iniMapSave(coor) {
-// 	window.alert('done writing to a file');
-// 	var txtFile = "/Users/buridiaditya/beklean/test.txt";
-// 	localStorage.setItem("latitude",coor.latitude);	
-// }
-	function initMap(position) {
-		var uluru = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-		var map = new google.maps.Map(document.getElementById('map'), {
-		  zoom: 4,
-		  center: uluru
-		});
-
-		var geocoder = new google.maps.Geocoder;
-        var infowindow = new google.maps.InfoWindow;
-        // iniMapSave(position.coords);
-        document.getElementById('Submit').addEventListener('click', function() {
-          geocodeLatLng(position,geocoder, map, infowindow);
-        });
+  function makeCookieAndSave(position){
     document.cookie = "lat = " + position.coords.latitude;
     document.cookie = "lang = " + position.coords.longitude;
-	}
-	function geocodeLatLng(position,geocoder, map, infowindow) {
-        var latlng = {lat: position.coords.latitude, lng: position.coords.longitude};
+    /// SAVE COOKIES INTO FILE // 
+  }
+  function cleaner(){
+    /// FILE STREAM ///
+    val latlng = {lat : 12 , lng: 12};
+    document.getElementById('1').addEventListener('click', function() {
+          setCleanerData(latlng);
+        });
+  }
+  function setCleanerData(latlng,id1){
+    var geocoder = new google.maps.Geocoder;
         geocoder.geocode({'location': latlng}, function(results, status) {
           if (status === 'OK') {
-            if (results[1]) {	
-              map.setZoom(11);
-              var marker = new google.maps.Marker({
+            if (results[1]) { 
+              document.getElementById(id1).innerHTML(results[1].formatted_address);
+            } else {
+              window.alert('No results found');
+            }
+          } else {
+            window.alert('Geocoder failed due to: ' + status);
+          }
+        });
+  }
+  function onClickCleaner(uluru){
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
                 position: latlng,
                 map: map
               });
+    var geocoder = new google.maps.Geocoder;
+    var infowindow = new google.maps.InfoWindow;
+    geocoder.geocode({'location': uluru}, function(results, status) {
+        if (status === 'OK') {
+            if (results[1]) { 
+              map.setZoom(11);
               infowindow.setContent(results[1].formatted_address);
               infowindow.open(map, marker);
             } else {
@@ -49,4 +57,4 @@ var x = document.getElementById("demo");
             window.alert('Geocoder failed due to: ' + status);
           }
         });
-    }
+  }
